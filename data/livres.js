c748ab892777d7aec10f83b143e74c59e4bcd5c5
gniselderf <gniselderf@gmail.com>
2020-06-17 22:42:33 -0400

    Quelques livres et une série test de plus

--- data/livres.js
+++ data/livres.js
@@ -14,6 +14,68 @@ let livres = [
         "pages": 150,
         "couverture": "/images/couvertures/adagio.jpg",
         "resume": "Faut pas mélanger magie et piano."
+    },
+    {
+        "titre": "En terres étrangères",
+        "auteur": "Robert Heinlein",
+        "progression": 0,
+        "pages": 150,
+        "couverture": "/images/couvertures/adagio.jpg",
+        "resume": "L'Homme de Mars."
+    },
+    {
+        "titre": "L'âge des étoiles",
+        "auteur": "Robert Heinlein",
+        "progression": 0,
+        "pages": 150,
+        "couverture": "/images/couvertures/adagio.jpg",
+        "resume": "Est-ce de l'inceste si c'est l'arrière-petite-fille de ton jumeau?"
+    },
+    {
+        "titre": "Sara",
+        "auteur": "Marion Zimmer Bradley",
+        "progression": 0,
+        "pages": 150,
+        "couverture": "/images/couvertures/adagio.jpg",
+        "resume": "Sara de Salem"
+    },
+    {
+        "titre": "La trahison des Dieux",
+        "auteur": "Marion Zimmer Bradley",
+        "progression": 0,
+        "pages": 150,
+        "couverture": "/images/couvertures/adagio.jpg",
+        "resume": "Violer une fille de 5 ans, franchement."
+    },
+    {
+        "titre": "La planète aux vents de folie",
+        "auteur": "Marion Zimmer Bradley",
+        "progression": 0,
+        "pages": 150,
+        "couverture": "/images/couvertures/adagio.jpg",
+        "serie": "Chronique de Ténébreuse",
+        "rang": 1,
+        "resume": "Kiriseth!"
+    },
+    {
+        "titre": "L'héritage d'Hastur",
+        "auteur": "Marion Zimmer Bradley",
+        "progression": 0,
+        "pages": 150,
+        "couverture": "/images/couvertures/adagio.jpg",
+        "serie": "Chronique de Ténébreuse",
+        "rang": 17,
+        "resume": "Finalement"
+    },
+    {
+        "titre": "La tour interdite",
+        "auteur": "Marion Zimmer Bradley",
+        "progression": 0,
+        "pages": 150,
+        "couverture": "/images/couvertures/adagio.jpg",
+        "serie": "Chronique de Ténébreuse",
+        "rang": 5,
+        "resume": "Je veux juste ton bonheur"
     }
 ]
 
